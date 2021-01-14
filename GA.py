3036803b219cee3ceda92749a8fced4a839e075e
tomoyuki-hiranuma <numanumatom@gmail.com>
2021-01-14 21:05:45 +0900

    sort_fitness Done

--- GA.py
+++ GA.py
@@ -1,169 +1,59 @@
-# coding: utf8
-import matplotlib.pyplot as plt
-import random
-import copy
+from Population import Population
+from NKModel import NKModel
 import numpy as np
 
-# パラメーター
+class GeneticAlgorithm:
+	def __init__(self, N, K, population_size):
+		self.population = Population(population_size, N)
+		self.nk_model = NKModel(N, K)
 
-ROUND = 8 # 総取り替えするであろう回数
-PARENTS_SIZE = 2
-POPULATION_SIZE = 100 # 集団の個体数
-GENERATION = POPULATION_SIZE * ROUND // PARENTS_SIZE # 世代数
-MUTATE_RATE = 0.01 # 突然異変の確率
+	def do_one_generation(self):
+		parent1_index = np.random.randint(len(self.population.array))
+		parent2_index = np.random.randint(len(self.population.array))
+		while parent1_index == parent2_index:
+			parent2_index = np.random.randint(len(self.population.array))
+		
+		parent1 = self.population.array[parent1_index]
+		parent2 = self.population.array[parent2_index]
+        # 交叉
+		child1, child2 = self.crossover(parent1, parent2)
+		# 突然変異
+		child1.mutation(0.1)
+		child2.mutation(0.1)
+		family = np.array([parent1, parent2, child1, child2])
+		selected_individual1, selected_individual2 = select_individuals(family)
 
-MAX_EVALUATION_NUMBER = 2*POPULATION_SIZE*10000
+	def crossover(self, parent1, parent2):
+		point = np.random.randint(1, self.individual_size)
+		child1 = Individual(self.individual_size)
+		child2 = Individual(self.individual_size)
+		child1.gene = parent1.gene[0:point] + parent2.gene[point:self.individual_size]
+		child2.gene = parent2.gene[0:point] + parent1.gene[point:self.individual_size]
+		return np.array[child1, child2]
+		
 
-def create_NK_landscape(N, K):
-    np.random.seed(1)
-    index = [ f'{i:0{K+1}b}' for i in range(2**(K+1)) ]
-    rand_array = np.random.random(2**(K+1))
-    return dict(zip(index, rand_array))
+	def sort_fitness(self, population):
+		population.print_array()
+		for individual in population.array:
+			individual.fitness = self.nk_model.calc_eval(individual.gene)
+		sorted_population = Population(len(population.array), len(population.array[0].gene))
+		sorted_population.array = sorted(population.array, key=lambda x: x.fitness)[::-1] #降順のインデックス
+		print("=====sorted====")
+		sorted_population.print_array()
+		return sorted_population
 
-# 適応度を計算する
-def calc_eval(gene, K):
-    fitness = 0.0
-    long_genes = gene + gene
-    for i in range(len(gene)):
-        fitness += NK_landscape[long_genes[i:i+K+1]]
-    fitness /= len(gene)
-    return fitness
+	def select_individuals(self, family):
+		sorted_family = sort_fitness(family)
+		elite_gene = sorted_family[0]
+		random_index = np.random.randint(1, len(sorted_family))
+		random_gene = sorted_family[random_index]
+		return elite_gene, random_gene
 
-# 集団を適応度順にソートする
-def sort_fitness(population, K):
-    fp = np.array([calc_eval(x, K) for x in population])
-    sorted_index = np.argsort(fp)[::-1] #降順のインデックス
-    sorted_population = population[sorted_index] #降順に並び替え
-    return sorted_population
 
-# 1点交叉
-def crossover(ind1, ind2, K):
-    r1 = random.randint(1, len(ind1))
-    child1 = copy.deepcopy(ind1[0:r1])
-    child2 = copy.deepcopy(ind2[0:r1])
-    child1 = child1 + ind2[r1:len(ind2)]
-    child2 = child2 + ind1[r1:len(ind1)]
-    # 突然変異
-    child1 = mutation(child1)
-    child2 = mutation(child2)
-    family = np.array([ind1, ind2, child1, child2])
-    sorted_family = sort_fitness(family, K)
-    elite_gene = sorted_family[0]
-    random_index = np.random.randint(1, len(sorted_family))
-    random_gene = sorted_family[random_index]
-    return elite_gene, random_gene
-
-# 突然変異（10%の確率で遺伝子を変化）
-def mutation(ind1):
-    ind2 = list(copy.deepcopy(ind1))
-    for i in range(N):
-        if random.random() < MUTATE_RATE:
-            ind2[i] = str(1 - int(ind2[i]))
-    ind2 = "".join(ind2)
-    return ind2
-
-def init_population(N):
-    return np.array([f'{np.random.randint(2**N):0{N}b}' for i in range(POPULATION_SIZE)]).astype(str)
-
-def do_one_generation(population, K):
-    r1 = random.randint(0, len(population) -1)
-    r2 = random.randint(0, len(population) -1)
-    while r1 == r2:
-        r2 = random.randint(0, len(population) -1)
-    # 交叉&突然変異
-    child1, child2 = crossover(population[r1], population[r2], K)
-    # 集団に追加
-    population[r1] = child1
-    population[r2] = child2
-    return population
-
-def print_population(population):
-    for individual in population:
-        print(individual)
-        
-def get_best_worst_evals(population, K):
-    better_eval = 0.0
-    worse_eval = 1.0
-    for individual in population:
-        fitness = calc_eval(individual, K)
-        if better_eval <= fitness:
-            better_eval = fitness
-        if worse_eval >= fitness:
-            worse_eval = fitness
-    return better_eval, worse_eval
-
-def get_optimization(N, K):
-    best_gene = ""
-    best_eval = 0.0
-    all_genes = np.array([ f'{i:0{N}b}' for i in range(2**(N)) ])
-    for gene in all_genes:
-        fitness = calc_eval(gene, K)
-        if best_eval <= fitness:
-            best_eval = fitness
-            best_gene = gene
-    return best_gene, best_eval
-  
-def get_mean_eval(population, K):
-    sum_eval = 0.0
-    for individual in population:
-        sum_eval += calc_eval(individual, K)
-    return sum_eval/len(population)
-
-# メイン処理
-NK_landscape = ""
-# 初期集団を生成（ランダムに0/1を10個ずつ並べる）
 if __name__ == '__main__':
-    N = 10 # 0/1リスト長（遺伝子長）
-    Ks = np.arange(0, N, 3)
-    for K in Ks:
-    # create NK model
-        print("K: {}のとき".format(K))
-        NK_landscape = create_NK_landscape(N, K)
-        print(NK_landscape)
-        BEST_GENE, BEST_EVAL = get_optimization(N, K)
+	N = 5
+	K = 0
+	Population_size = 10
 
-        # GA開始
-        population = init_population(N)
-        print("0世代")
-        print_population(population)
-        generation_count = 0
-        eval_number = PARENTS_SIZE * generation_count
-        best_eval = 0.0
-
-        generations = []
-        elites_evals = []
-        mean_evals = []
-        worst_evals = []
-        mean_eval = 0.0
-        while eval_number < MAX_EVALUATION_NUMBER and BEST_EVAL-mean_eval >= 0.001:
-            print(str(generation_count + 1) + u"世代")
-            population = do_one_generation(population, K)
-            best_eval, worst_eval = get_best_worst_evals(population, K)
-        
-            generation_count += 1
-            eval_number = POPULATION_SIZE * generation_count
-            mean_eval = get_mean_eval(population, K)
-
-            print("best evaluation: {}".format(best_eval))
-            print("mean eval: {}".format(mean_eval))
-            print("worst eval: {}".format(worst_eval))
-
-            ### 出力用
-            generations.append(eval_number)
-            elites_evals.append(best_eval)
-            mean_evals.append(mean_eval)
-            worst_evals.append(worst_eval)
-        
-        print("opt gene: {}\nopt evaluation: {}".format(BEST_GENE, BEST_EVAL))
-        ## グラフ
-        plt.plot(generations, elites_evals, label="best eval in population")
-        plt.plot(generations, mean_evals, label="mean eval in population")
-        plt.plot([0, eval_number], [BEST_EVAL, BEST_EVAL], label="Opt Evaluation", color='red', linestyle='--')
-        plt.plot(generations, worst_evals, label="bad eval in population")
-        plt.legend()
-        plt.xlabel("number of evaluation")
-        plt.ylabel("evaluation value")
-        plt.title("NK model N={} K={} POP_SIZE={}".format(N, K, POPULATION_SIZE))
-        plt.savefig("./NK_Model_png/N={}_K={}_POP_SIZE={}.png".format(N, K, POPULATION_SIZE))
-        plt.clf()
-    # plt.show()
+	ga = GeneticAlgorithm(N, K, Population_size)
+	print(ga.sort_fitness(ga.population))
--- Population.py
+++ Population.py
@@ -1,38 +1,13 @@
 import numpy as np
 import copy
 from Individual import Individual
+from NKModel import NKModel
 
 class Population:
 	def __init__(self, population_size, individual_size):
 		self.individual_size = individual_size
 		self.array = np.array([ Individual(individual_size) for i in range(population_size)])
 
-	def crossover(self):
-		point = np.random.randint(1, self.individual_size)
-		parent1_index = np.random.randint(len(self.array))
-		parent2_index = np.random.randint(len(self.array))
-		while parent1_index == parent2_index:
-			parent2_index = np.random.randint(len(self.array))
-
-		parent1 = self.array[parent1_index]
-		parent2 = self.array[parent2_index]
-		child1 = Individual(self.individual_size)
-		child2 = Individual(self.individual_size)
-		child1.gene = parent1.gene[0:point] + parent2.gene[point:self.individual_size]
-		child2.gene = parent2.gene[0:point] + parent1.gene[point:self.individual_size]
-		# 突然変異
-		child1.mutation(0.1)
-		child2.mutation(0.1)
-		return np.array([parent1.gene, parent2.gene, child1.gene, child2.gene])
-		# family = np.array([parent1, parent2, child1, child2])
-		# sorted_family = sort_fitness(family)
-		# elite_gene = sorted_family[0]
-		# random_index = np.random.randint(1, len(sorted_family))
-		# random_gene = sorted_family[random_index]
-		# return elite_gene, random_gene
-
-	def do_one_generation(self):
-		pass
 
 	def print_array(self):
 		for i in range(len(self.array)):
@@ -46,6 +21,6 @@ if __name__ == '__main__':
 	population = Population(pop_size, ind_size)
 	population.print_array()
 	print("crossover")
-	print(population.crossover())
-	# print(population.array[1].gene)
-	# print(population.mutation(population.array[1].gene))
+	family = population.crossover()
+	for ind in family:
+		print(ind.gene)
